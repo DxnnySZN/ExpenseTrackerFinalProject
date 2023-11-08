7da797c53d4fbde9934751309a43335ecfbde9ba
yoondaniel1 <yoondaniel2006@gmail.com>
2023-11-08 08:22:09 -0600

    finished activity_first_screen 11/8/23
    make logo with logo.com and set the ImageView to that logo + work on activity_first_screen_info1 and activity_first_screen_info2

---
+++ app/src/main/java/com/example/expensetrackerfinalproject/FirstScreenInfo1.java
@@ -0,0 +1,14 @@
+package com.example.expensetrackerfinalproject;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class FirstScreenInfo1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_first_screen_info1);
+    }
+}
