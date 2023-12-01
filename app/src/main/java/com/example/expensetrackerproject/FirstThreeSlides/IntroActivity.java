8df3c9ced90c7edebad783c522d2b46f53ee0be9
yoondaniel1 <yoondaniel2006@gmail.com>
2023-12-01 08:22:38 -0600

    worked on IntroActivity 12/1/23
    there's probably an AndroidManifest issue

--- app/src/main/java/com/example/expensetrackerproject/FirstThreeSlides/IntroActivity.java
+++ app/src/main/java/com/example/expensetrackerproject/FirstThreeSlides/IntroActivity.java
@@ -1,4 +1,36 @@
 package com.example.expensetrackerproject.FirstThreeSlides;
 
-public class IntroActivity {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+import com.example.expensetrackerproject.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntroActivity extends AppCompatActivity {
+    private ViewPager screenPager;
+    IntroViewPagerAdapter introViewPagerAdapter;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+
+        int resourceID1 = getResources().getIdentifier("dudewithbread", "drawable", getPackageName());
+        int resourceID2 = getResources().getIdentifier("testpic", "drawable", getPackageName());
+        int resourceID3 = getResources().getIdentifier("aimoneylastslide", "drawable", getPackageName());
+
+        // fill list screen
+        List<ScreenActivity> mList = new ArrayList<>();
+        mList.add(new ScreenActivity("Budget Like a Pro: Spend Smartly, Save Effortlessly", resourceID1));
+        mList.add(new ScreenActivity("Track, Budget, Save – Your Financial Roadmap", resourceID2));
+        mList.add(new ScreenActivity("Start Tracking Your Expenses Today!", resourceID3));
+
+        // setup ViewPager
+        screenPager = findViewById(R.id.ViewPagerScreen);
+        introViewPagerAdapter = new IntroViewPagerAdapter(this, mList);
+        screenPager.setAdapter(introViewPagerAdapter);
+    }
 }
